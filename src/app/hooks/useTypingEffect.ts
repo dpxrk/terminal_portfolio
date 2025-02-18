582bb3191a7d7e12083c61f569033871bbe4c7fd
dpxrk <danielpark0503@gmail.com>
2025-02-18 18:38:05 -0500

    useTypingEffect

---
+++ src/app/hooks/useTypingEffect.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export const useTypingEffect = (text: string, speed: number = 30) => {
+  const [displayedText, setDisplayedText] = useState("");
+  const [isComplete, setIsComplete] = useState(false);
+
+  useEffect(() => {
+    let index = 0;
+    setIsComplete(false);
+    setDisplayedText("");
+
+    const timer = setInterval(() => {
+      if (index < text.length) {
+        setDisplayedText((prev) => prev + text.charAt(index));
+        index++;
+      } else {
+        setIsComplete(true);
+        clearInterval(timer);
+      }
+    }, speed);
+
+    return () => clearInterval(timer);
+  }, [text, speed]);
+
+  return { displayedText, isComplete };
+};
